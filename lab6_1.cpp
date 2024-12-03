02bfc954136d9267d514fed2163136596d61843b
Pongpx <yannafong555@gmail.com>
2024-12-03 14:58:27 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 
 int main(){
+    int integ;
+    int evecount = 0;
+    int oddcount = 0;
+
+    do {
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> integ;
+
+    if (integ != 0) {
+            if (integ % 2 == 0) {
+                evecount++;
+            } else {
+                oddcount++;
+            }
+        }
+    } while (integ != 0);
+
+    cout << "#Even numbers = \n" << evecount;
+    cout << "#Odd numbers = "  << oddcount;
+
+
     return 0;
 }
+
